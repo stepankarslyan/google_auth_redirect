cc8a91a5c125d420fe33fe4821015bd614ce66c0
Stepan Karslyan <stepan.karslyan@gmail.com>
2014-06-20 11:41:59 +0400

    making google api request

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var googleapis = require("googleapis");
+var app = express();
+
+var OAuth2 = googleapis.auth.OAuth2;
+
+var oauth2Client = new OAuth2(
+	'xx', 
+	'xx', 
+	'http://localhost:3000/oauth/google/callback');
+
+
+app.get("/", function(req, res) {
+
+  var scopes = ['https://www.googleapis.com/auth/calendar'];
+
+	var url = oauth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: scopes.join(" ")
+	});
+
+  res.redirect(url);
+});
+
+var server = app.listen(3001, function() {
+  console.log("Listening on port 3001...");
+});
+
